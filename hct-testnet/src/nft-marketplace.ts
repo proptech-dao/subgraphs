965602dfa5b761d7fc36774bf29aff02229b6fdf
pedro bufulin <pedro.almeida@ufu.br>
2023-08-22 19:17:56 -0300

    nft market listing tests and handlers

--- hct-testnet/src/nft-marketplace.ts
+++ hct-testnet/src/nft-marketplace.ts
@@ -1,13 +1,19 @@
+import { Bytes } from "@graphprotocol/graph-ts"
 import {
   NFTListed as NFTListedEvent,
   NFTPurchased as NFTPurchasedEvent,
   NFTUnlisted as NFTUnlistedEvent
 } from "../generated/NFTMarketplace/NFTMarketplace"
-import { NFTListed, NFTPurchased, NFTUnlisted } from "../generated/schema"
+import { NFTInMarket, NFTListed, NFTPurchased, NFTUnlisted } from "../generated/schema"
+import { store } from '@graphprotocol/graph-ts'
 
 export function handleNFTListed(event: NFTListedEvent): void {
   let entity = new NFTListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.tokenId))  
+    )
+
+  let nftListed = new NFTInMarket(
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.tokenId))  
   )
 
   entity.nftAddress = event.params.nftAddress
@@ -19,13 +25,25 @@ export function handleNFTListed(event: NFTListedEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
+  nftListed.tokenId = event.params.tokenId
+  nftListed.nftAddress = event.params.nftAddress
+  nftListed.price = event.params.price
+  nftListed.paymentToken = event.params.paymentToken
+
+
   entity.save()
+  nftListed.save()
 }
 
 export function handleNFTPurchased(event: NFTPurchasedEvent): void {
   let entity = new NFTPurchased(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+
+  let nftListed =  NFTInMarket.load(
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.tokenId))  
+  )
+
   entity.nftAddress = event.params.nftAddress
   entity.tokenId = event.params.tokenId
   entity.buyer = event.params.buyer
@@ -35,6 +53,11 @@ export function handleNFTPurchased(event: NFTPurchasedEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
+  if(nftListed){
+    nftListed.buyer = event.params.buyer
+    nftListed.save()
+}
+
   entity.save()
 }
 
@@ -42,12 +65,18 @@ export function handleNFTUnlisted(event: NFTUnlistedEvent): void {
   let entity = new NFTUnlisted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+
+  store.remove(
+    "NFTInMarket",
+    Bytes.fromByteArray(Bytes.fromBigInt(event.params.tokenId)).toHexString()  
+    )
+
   entity.nftAddress = event.params.nftAddress
   entity.tokenId = event.params.tokenId
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-
+  
   entity.save()
 }
--- hct-testnet/tests/nft-marketplace.test.ts
+++ hct-testnet/tests/nft-marketplace.test.ts
@@ -9,8 +9,8 @@ import {
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
 import { NFTListed } from "../generated/schema"
 import { NFTListed as NFTListedEvent } from "../generated/NFTMarketplace/NFTMarketplace"
-import { handleNFTListed } from "../src/nft-marketplace"
-import { createNFTListedEvent } from "./nft-marketplace-utils"
+import { handleNFTListed, handleNFTPurchased, handleNFTUnlisted } from "../src/nft-marketplace"
+import { createNFTListedEvent, createNFTPurchasedEvent, createNFTUnlistedEvent } from "./nft-marketplace-utils"
 import { log } from "matchstick-as/assembly/log";
 
 // Tests structure (matchstick-as >=0.5.0)
@@ -26,7 +26,7 @@ describe("Describe entity assertions", () => {
     let tokenId = BigInt.fromI32(234)
     let price = BigInt.fromI32(234)
     let paymentToken = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
+      "0x0000000000000000000000000000000000000002"
     )
     let newNFTListedEvent = createNFTListedEvent(
       nftAddress,
@@ -34,8 +34,7 @@ describe("Describe entity assertions", () => {
       price,
       paymentToken
     )
-     id = newNFTListedEvent.transaction.hash.concatI32(newNFTListedEvent.logIndex.toI32())
-      log.warning(id.toString(), [])
+     id =  Bytes.fromByteArray(Bytes.fromBigInt(newNFTListedEvent.params.tokenId))  
     handleNFTListed(newNFTListedEvent)
   })
 
@@ -74,7 +73,7 @@ describe("Describe entity assertions", () => {
       "NFTListed",
       id.toHexString(),
       "paymentToken",
-      "0x0000000000000000000000000000000000000001"
+      "0x0000000000000000000000000000000000000002"
     )
 
     // More assert options:
@@ -82,5 +81,61 @@ describe("Describe entity assertions", () => {
   })
 
 
+  test("NFTMInMkarket object after listing", () =>{
+    assert.entityCount("NFTInMarket", 1)
+    assert.fieldEquals(
+      "NFTInMarket",
+      id.toHexString(),
+      "paymentToken",
+      "0x0000000000000000000000000000000000000002"
+    )
+  })
+
+
+  test("NFTInMarket object after buying", () =>{
+    let nftAddress = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let tokenId = BigInt.fromI32(234)
+    let price = BigInt.fromI32(234)
+    let buyer = Address.fromString(
+      "0x0000000000000000000000000000000000000002"
+    )
+    let newNFTListedEvent = createNFTPurchasedEvent(
+      nftAddress,
+      tokenId,
+      buyer,
+      price
+    )
+    handleNFTPurchased(newNFTListedEvent)
+    assert.entityCount("NFTInMarket", 1)
+    assert.entityCount("NFTPurchased", 1)
+  
+
+    assert.fieldEquals(
+      "NFTInMarket",
+      id.toHexString(),
+      "buyer",
+      "0x0000000000000000000000000000000000000002"
+    )
+
+    
+  })
+
+
+  test("NFTInMarket has to be removed after un-listing", () =>{
+
+    let nftAddress = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let tokenId = BigInt.fromI32(234)
+    let newNFTListedEvent = createNFTUnlistedEvent(
+      nftAddress,
+      tokenId,
+    )
+    handleNFTUnlisted(newNFTListedEvent)
+    assert.entityCount("NFTInMarket", 0)
+
+  })
 
 })
